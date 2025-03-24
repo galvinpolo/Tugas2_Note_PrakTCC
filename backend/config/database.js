11c355b20673b195ac6ac39ac15f5286c6fb623e
galvin <162205216+galvinpolo@users.noreply.github.com>
2025-03-24 21:22:51 +0700

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,8 +1,20 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize("notegalpin", "root", "", {
-  host: "localhost", 
+const db = new Sequelize("notes", "root", "", {
+  host: "34.56.13.236",
   dialect: "mysql",
 });
+
+const testConnection = async () => {
+  try {
+    await db.authenticate();
+    console.log("Database terhubung!");
+  } catch (error) {
+    console.error("Gagal terhubung ke database:", error.message);
+    throw error;
+  }
+};
+
+testConnection();
 
 export default db;

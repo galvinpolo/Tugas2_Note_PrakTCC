233b972d1e8f6e20f92bdc5888aeafa34cccd301
Your Name <you@example.com>
2025-05-22 11:33:51 +0700

    console log db

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,6 +3,13 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+console.log("Connecting to DB with:", {
+  DB_NAME: process.env.DB_NAME,
+  DB_USERNAME: process.env.DB_USERNAME,
+  DB_PASSWORD: process.env.DB_PASSWORD,
+  DB_HOST: process.env.DB_HOST,
+});
+
 const db = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USERNAME,
